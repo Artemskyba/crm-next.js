f2b10f64724dd501ec0f4c32f8b5cffaf9e32584
Artemskyba <artemgennadievich@icloud.com>
2024-07-07 11:49:23 +0300

    add some components

---
+++ src/app/components/add-company-button.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Button } from './button';
+import dynamic from 'next/dynamic';
+
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+  ssr: false,
+});
+
+export function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}></Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import StatusLabel, { Status } from './components/status-label/status-label';
+// import StatusLabel, { Status } from './components/status-label';
+import { AddCompanyButton } from './components/add-company-button';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl text-green-600">HELLO WORLD</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      {/* <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
+      <AddCompanyButton />
     </main>
   );
 }
